02ab69b7d6a0a8c82fcd55cf8308cfba854c84a9
javaguides <aditya.kumargupta@UNISYS.com>
2023-04-20 15:22:20 +0530

    adding two number committed in repository

---
+++ javaBasics/src/javaBasicsDay1/AddingTwoNumber.java
@@ -0,0 +1,15 @@
+package javaBasicsDay1;
+
+public class AddingTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=2;
+		int b=3;
+		
+		
+		System.out.println(a+b);
+	}
+
+}
