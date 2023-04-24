d025a45c0fffea62e8782dd14b913b130d997a68
PriyankaShastri11 <130047943+PriyankaShastri11@users.noreply.github.com>
2023-04-24 16:05:27 +0530

    Committed assignments

---
+++ javaBasics/src/PriyankaAssessments/LoginFunctionality.java
@@ -0,0 +1,28 @@
+package PriyankaAssessments;
+
+public class LoginFunctionality {
+	public static void main (String[] args){
+		String username = "Abc";
+		String password = "Abc@123";
+		
+		
+		if (!username.equals("Abcd"))
+		{
+		System.out.println("Username is incorrect");
+	}
+		else{
+			System.out.println("Username is correct");
+			
+	}
+		if (!password.equals("Abc@123"))
+		{
+		System.out.println("password is incorrect");
+	}
+		else{
+			System.out.println("password is correct");
+			
+	}
+	}
+}
+
+
