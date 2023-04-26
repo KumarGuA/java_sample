75d3f6ed66821917b9229efe268fd103e9c155cb
javaguides <aditya.kumargupta@UNISYS.com>
2023-04-26 13:39:50 +0530

    update modified structure

---
+++ javaBasics/src/ControlStatements/BhoomikaAssesments/Relational.java
@@ -0,0 +1,14 @@
+package ControlStatements.BhoomikaAssesments;
+
+public class Relational {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=55,b=45;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+	}
+
+}
---
+++ javaBasics/src/ControlStatements/PriyankaAssessments/CalculatorFunctionality.java
@@ -0,0 +1,34 @@
+package ControlStatements.PriyankaAssessments;
+
+public class CalculatorFunctionality {
+	public static void main (String[]args)
+	{
+		
+		int a=10, b=30;
+
+		int option = 1;
+		switch (option)
+		{
+		case 1:{
+			System.out.println("a + b = " + (a + b));
+		}
+		case 2:{
+			System.out.println("a * b = " + (a * b));
+		}
+		case 3:{
+			System.out.println("a / b = " + (a / b));
+		}
+		case 4:{
+			System.out.println("a -b = " + (a - b));
+		}
+		}
+	}
+}
+
+		
+		
+		
+
+
+
+
