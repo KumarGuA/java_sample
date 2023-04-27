583096626ce8ec1f908d95b6f07851efb0a8ea9f
bhoomika611 <bhoomika.hm@unisys.com>
2023-04-27 18:12:36 +0530

    commit assesment

--- javaBasics/src/BhoomikaAssesments/Student.java
+++ javaBasics/src/BhoomikaAssesments/Student.java
@@ -1,35 +1,36 @@
 package BhoomikaAssesments;
 
-class StudentDetails {
+public class Student {
 	String Name;
 	int rollNo;
-}
+	int Mark1;
+	int Mark2;
+	int Mark3;
+	int TotalMarks;
 
-class Marks {
-	int English = 99;
-	int Maths = 95;
-	int CS = 100;
-	int TotalMarks = (English + Maths + CS);
+	public void calculateTotal() {
+		TotalMarks = (Mark1 + Mark2 + Mark3);
+	}
 
-}
+	public void displayStudDetails(String name, int RollNo, int mar1, int mar2, int mar3) {
 
-public class Student {
+		Name = name;
+		rollNo = RollNo;
+		Mark1 = mar1;
+		Mark2 = mar2;
+		Mark3 = mar3;
+		System.out.println("Student name :" + name);
+		System.out.println("Student ID : " + RollNo);
+		System.out.println("Total Marks " + (Mark1 + Mark2 + Mark3));
+	}
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
-
-		StudentDetails obj1 = new StudentDetails();
-		obj1.Name = "Bhoomika";
-		obj1.rollNo = 123;
-		System.out.println("Student Name is : " + obj1.Name);
-		System.out.println("Student Roll No is :" + obj1.rollNo);
-
-		Marks obj2 = new Marks();
-		// obj2.TotalMarks(99+90+100);
-		System.out.println("Total Marks is :" + obj2.TotalMarks);
-
-		// int per = (obj2.TotalMarks/300*100);
-		System.out.println("Percentage:" + obj2.TotalMarks * 100 / 300);
+		Student obj = new Student();
+		obj.displayStudDetails("Bhoomika", 12, 99, 89, 89);
+		obj.calculateTotal();
 
 	}
+
 }
