4b466c44659d8535b40f9a8b74a47b71352733f6
javaguides <aditya.kumargupta@UNISYS.com>
2023-05-05 14:33:01 +0530

    Inheritance code updated

---
+++ javaBasics/src/Oops_Inheritence/D.java
@@ -0,0 +1,25 @@
+//package Oops_Inheritence;
+//
+//class g 
+//{ 
+// void msg() 
+// { 
+//    System.out.println("Hello Java"); 
+//  } 
+//} 
+//class f 
+//{ 
+//  void msg() 
+//  { 
+//     System.out.println("Welcome you"); 
+//  } 
+// } 
+//class D extends g
+//{ 
+// // Suppose if it is.
+// public static void main(String args[]) 
+// { 
+//   D obj = new D(); 
+//   obj.msg();//Now which msg() method would be called? 
+//  } 
+// }
