6ea461af6f94efb732f076e314e859e90707a3a5
bhoomika611 <bhoomika.hm@unisys.com>
2023-05-30 16:18:05 +0530

    committed char to string and string,int of array programs

---
+++ javaBasics/src/ControlStatements/BhoomikaAssesments/CharToString.java
@@ -0,0 +1,17 @@
+package ControlStatements.BhoomikaAssesments;
+
+public class CharToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "Bhoomika";
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			System.out.println(c);
+		
+
+		}
+	}
+}
