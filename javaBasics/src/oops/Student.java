95c4ede9c50785857a38ef1f3f854c58362ff3a1
bhoomika611 <bhoomika.hm@unisys.com>
2023-06-01 12:09:50 +0530

    committed string and array with oops concept program

---
+++ javaBasics/src/oops/Student.java
@@ -0,0 +1,32 @@
+package oops;
+class Gender {
+	public void gender() {
+		System.out.println("Gender : Female");
+	}
+	
+}
+public class Student extends Gender {
+	private String name;
+	private int age;
+
+	Student() {
+		name = "noname";
+		age = 1;
+	}
+
+	Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	void setNameAndAge(String name, int age) {
+		this.name = name;
+		this.age = age;
+
+	}
+
+	void introduce() {
+		System.out.println("Name : " + name + "\nAge :" + age);
+	}
+
+}
